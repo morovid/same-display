2a2b547ca32539fc4d0287caab719be6c64fb708
Jan Malčák <services@malcak.cz>
2026-01-08 09:08:43 +0100

    Initial brew cask

---
+++ homebrew.rb
@@ -0,0 +1,11 @@
+cask "same-display" do
+  version "1.0"
+  sha256 "d951e03393642b9aed20efb41afbb522ea8bb00f7cff51213bccca61828e8712"
+
+  url "https://github.com/morovid/same-display/releases/download/v1.0/SameDisplay_v1.0.dmg"
+  name "Same Display"
+  desc "Automatically moves newly opened windows to the display where your mouse cursor is located"
+  homepage "https://same-display.morovid.com"
+
+  app "SameDisplay.app"
+end
